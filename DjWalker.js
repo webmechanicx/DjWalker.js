d7feac3b084de60e9d234be09c8cd696c45a8f5d
Farhadur Rahim <varendrasoft@gmail.com>
2015-04-25 14:24:35 +0600

    Update DjWalker.js

--- DjWalker.js
+++ DjWalker.js
@@ -1,4 +1,4 @@
-/* MIT License Non-Commercial and Commercial use, You can use this code anywhere including any number projects you may require. Just put my name as reference.
+/* MIT License Non-Commercial and Commercial use, You can use this code anywhere including any number projects you may require. You can give my name as reference only if you wish.
    Author: Farhadur Rahim Email: webmechanicx@gmail.com
 */
 
